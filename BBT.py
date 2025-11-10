b7914be17efed2d0974128a46e0469c726bed29d
minhhieu90999099 <155273346+minhhieu90999099@users.noreply.github.com>
2025-11-10 10:24:39 +0700

    BBT.py

--- BBT.py
+++ BBT.py
@@ -4,7 +4,8 @@ components.html("""
                 <!DOCTYPE html>
 <html lang="vi">
 <head>
-    <script src="https://raw.githubusercontent.com/minhhieu90999099/bangbienthien/main/ScriptBBT.js"></script>
+    
+    <script type="module" crossorigin src="https://minhnganedu.com/bangbienthien/assets/index-C_tTioXE.js"></script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="robots" content="index, follow">
@@ -197,3 +198,4 @@ components.html("""
 </html>
 
 """, height=1000, width=3000)
+
