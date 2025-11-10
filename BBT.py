36c55263d91906af689a57cbe8c13baf80cb0c94
minhhieu90999099 <155273346+minhhieu90999099@users.noreply.github.com>
2025-11-10 10:04:52 +0700

    BBT.py

--- BBT.py
+++ BBT.py
@@ -5,10 +5,10 @@ import streamlit.components.v1 as components
 # Bước 1: Đọc nội dung file HTML
 with open("VeBangBienThien.html", "r", encoding="utf-8") as f:
     html_body = f.read()
+# Đọc file JS từ GitHub bằng requests
+js_url = "https://raw.githubusercontent.com/minhhieu90999999/bangbienthien/main/scriptBBT.js"
+js_code = requests.get(js_url).text
 
-# Bước 2: Nếu cần thêm JavaScript, đọc từ file JS
-with open("https://raw.githubusercontent.com/minhhieu90999099/bangbienthien/main/ScriptBBT.js", "r", encoding="utf-8") as f:
-    js_code = f.read()
 
 # Bước 3: Tạo mã HTML hoàn chỉnh
 html_code = f"""
@@ -27,6 +27,7 @@ html_code = f"""
 
 # Bước 4: Hiển thị trong Streamlit
 components.html(html_code, height=1200, width=3000)
+
 
 
 
