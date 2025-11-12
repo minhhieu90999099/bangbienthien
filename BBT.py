d79b55597ff40e1376cb925b0f0ea3d2ba6b7bf7
minhhieu90999099 <155273346+minhhieu90999099@users.noreply.github.com>
2025-11-12 19:52:24 +0700

    BBT.py

--- BBT.py
+++ BBT.py
@@ -1,3 +1,6 @@
+import streamlit as st
+import streamlit.components.v1 as components
+components.html("""
 <html lang="vi"><head>
 <style>
   html, body {
@@ -8,8 +11,8 @@
     overflow: hidden;
   }
 </style>
- <script type="module" crossorigin src="https://minhnganedu.com/bangbienthien/assets/index-C_tTioXE.js"></script>
-
+ 
+ <script type="module" crossorigin="" src="https://congenial-carnival-9774jwpq5vvpf997w.github.dev/workspaces/bangbienthien/ScriptBBT_101125.js"></script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="robots" content="index, follow">
@@ -243,7 +246,6 @@
       })();
     </script>
   
-</body></html>
+</body><en2vi-host class="corom-element" version="3" style="all: initial; position: absolute; top: 0; left: 0; right: 0; height: 0; margin: 0; text-align: left; z-index: 10000000000; pointer-events: none; border: none; display: block"></en2vi-host><savior-host style="all: unset; position: absolute; top: 0; left: 0; z-index: 99999999999999; display: block !important; overflow: unset"></savior-host></html>
 
-
-
+""", height = 4000, width = 4000)
