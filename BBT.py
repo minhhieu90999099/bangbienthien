386a5e3b2dbd0919a1f684c5ad63f7b1806dad7d
minhhieu90999099 <155273346+minhhieu90999099@users.noreply.github.com>
2025-11-12 19:42:26 +0700

    BBT.py

--- BBT.py
+++ BBT.py
@@ -9,7 +9,7 @@
   }
 </style>
  <script type="module" crossorigin="" src="https://minhnganedu.com/bangbienthien/assets/index-C_tTioXE.js"></script>
- <script type="module" crossorigin src="/-/build/assets/index-B59N3yFD.js"></script>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="robots" content="index, follow">
@@ -244,3 +244,4 @@
     </script>
   
 </body><en2vi-host class="corom-element" version="3" style="all: initial; position: absolute; top: 0; left: 0; right: 0; height: 0; margin: 0; text-align: left; z-index: 10000000000; pointer-events: none; border: none; display: block"></en2vi-host><savior-host style="all: unset; position: absolute; top: 0; left: 0; z-index: 99999999999999; display: block !important; overflow: unset"></savior-host></html>
+
